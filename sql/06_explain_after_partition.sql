c43ad7360494e50e6b330ebfba24e53a3ef76cf6
Tog91l <114440739+Tog91l@users.noreply.github.com>
2026-03-27 15:40:17 +0300

    Update 06_explain_after_partition.sql

--- sql/06_explain_after_partition.sql
+++ sql/06_explain_after_partition.sql
@@ -25,6 +25,45 @@ SET work_mem = '32MB';
 
 \echo '--- Q3 ---'
 -- TODO: EXPLAIN (ANALYZE, BUFFERS) ...
+EXPLAIN (ANALYZE, BUFFERS)
+SELECT id, user_id, total_amount, created_at
+FROM orders_new
+WHERE total_amount > 10000
+ORDER BY created_at DESC
+LIMIT 20;
 
+\echo '--- Q2: Фильтрация по статусу + диапазону дат ---'
+-- TODO: Подставьте свой запрос
+-- EXPLAIN (ANALYZE, BUFFERS)
+-- SELECT ...
+-- FROM orders
+-- WHERE status = 'paid'
+--   AND created_at >= ...
+--   AND created_at < ...;
+EXPLAIN(ANALYZE, BUFFERS)
+SELECT id, user_id , total_amount, created_at 
+FROM orders_new 
+WHERE status = 'paid' AND created_at >= '2025-08-01' AND created_at < '2025-12-31'
+ORDER BY created_at;
+
+\echo '--- Q3: JOIN + GROUP BY ---'
+-- TODO: Подставьте свой запрос
+-- EXPLAIN (ANALYZE, BUFFERS)
+-- SELECT ...
+-- FROM orders o
+-- JOIN order_items oi ON oi.order_id = o.id
+-- WHERE ...
+-- GROUP BY ...
+-- ORDER BY ...
+-- LIMIT ...;
+EXPLAIN(ANALYZE, BUFFERS)
+SELECT o.user_id, COUNT(*) AS orders_count, SUM(o.total_amount) AS total_spent
+FROM orders_new o
+JOIN order_items oi ON oi.order_id = o.id
+WHERE o.created_at >= '2025-01-01'
+GROUP BY o.user_id
+ORDER BY total_spent DESC
+LIMIT 15;
 -- (Опционально) Q4
 -- TODO
+
