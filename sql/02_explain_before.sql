f84072c81399be6713f564525a31a379d7cae90b
Tog91l <114440739+Tog91l@users.noreply.github.com>
2026-03-27 15:38:12 +0300

    Update 02_explain_before.sql

--- sql/02_explain_before.sql
+++ sql/02_explain_before.sql
@@ -20,6 +20,13 @@ ANALYZE;
 -- WHERE ...
 -- ORDER BY created_at DESC
 -- LIMIT ...;
+--поиск дорогих заказов 
+EXPLAIN (ANALYZE, BUFFERS)
+SELECT id, user_id, total_amount, created_at
+FROM orders
+WHERE total_amount > 10000
+ORDER BY created_at DESC
+LIMIT 20;
 
 \echo '--- Q2: Фильтрация по статусу + диапазону дат ---'
 -- TODO: Подставьте свой запрос
@@ -29,6 +36,11 @@ ANALYZE;
 -- WHERE status = 'paid'
 --   AND created_at >= ...
 --   AND created_at < ...;
+EXPLAIN(ANALYZE, BUFFERS)
+SELECT id, user_id , total_amount, created_at 
+FROM orders 
+WHERE status = 'paid' AND created_at >= '2025-08-01' AND created_at < '2025-12-31'
+ORDER BY created_at;
 
 \echo '--- Q3: JOIN + GROUP BY ---'
 -- TODO: Подставьте свой запрос
@@ -40,5 +52,13 @@ ANALYZE;
 -- GROUP BY ...
 -- ORDER BY ...
 -- LIMIT ...;
-
+EXPLAIN(ANALYZE, BUFFERS)
+SELECT o.user_id, COUNT(*) AS orders_count, SUM(o.total_amount) AS total_spent
+FROM orders o
+JOIN order_items oi ON oi.order_id = o.id
+WHERE o.created_at >= '2025-01-01'
+GROUP BY o.user_id
+ORDER BY total_spent DESC
+LIMIT 15;
 -- (Опционально) Q4: полный агрегат по периоду, который сложно ускорить индексами
+
